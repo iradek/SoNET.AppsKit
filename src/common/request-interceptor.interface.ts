79e7c5605cfd6903909d6c2bcd73094c8b652333
iradek <iradek@iradek.com>
2017-10-21 19:08:51 -0700

    Fixed linting problems

--- src/common/request-interceptor.interface.ts
+++ src/common/request-interceptor.interface.ts
@@ -1,3 +1,5 @@
+import {Headers} from '@angular/http'
+
 export interface IRequestInterceptor {
   beforeRequestAsync(url: string, method: string, data: any, headers: Headers): Promise<boolean>;
 }
--- src/common/response-interceptor.interface.ts
+++ src/common/response-interceptor.interface.ts
@@ -1,3 +1,5 @@
+import {Headers} from '@angular/http'
+
 export interface IResponseInterceptor {
   afterResponseAsync(response: any, url: string, method: string, data: any, headers: Headers): Promise<boolean>;
 }
